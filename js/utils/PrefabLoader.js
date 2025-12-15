6e048c0f5fc9137f037033e292ef8c07fe925168
Elijah De Lima <moreskilltreegames@gmail.com>
2025-12-15 19:55:34 +0000

    error fixed

--- js/utils/PrefabLoader.js
+++ js/utils/PrefabLoader.js
@@ -35,10 +35,6 @@ export default class PrefabLoader {
         for (const [key, def] of Object.entries(data)){
             // create minimal entity; position/size will be set at spawn time
             const ent = new Entity(new Vector(0, -16), new Vector(16,16));
-            // Apply any top-level data from the prefab (e.g., health, team)
-            for (const [k, v] of Object.entries(def.extra)) {
-                ent[k] = v;
-            }
             const comps = def.components;
             for (const [compKey, compData] of Object.entries(comps)){
                 // Get the module & create an instance
